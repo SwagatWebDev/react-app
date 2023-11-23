d36f808c7aa77898cf9f66b11aaaca14bbe32bf0
swagat030 <swagatkumarmishra888@gmail.com>
2023-11-24 03:06:09 +0530

    File Updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,8 @@ import Shimmer from "./Shimmer";
 const Body = () => {
     // Local State Variable
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    console.log("Body Rendered");
     useEffect(() => {
         fetchData();
     }, []);
@@ -13,16 +15,33 @@ const Body = () => {
         const API_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9165757&lng=77.6101163&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
         const data = await fetch(API_URL);
         const response = await data.json();
+        console.log(response);
         const restaurantListData = response?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         setListOfRestaurants(restaurantListData);
     }
-    if (listOfRestaurants.length === 0){
-        // return (<h1>Loading...</h1>);
+    // Conditional Rendering
+    /*if (listOfRestaurants.length === 0){
         return (<Shimmer/>)
-    }
-    return (
+    }*/
+    return listOfRestaurants.length === 0 ? (<Shimmer/>) : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box"
+                           value={searchText}
+                           onChange={(e) => {
+                               setSearchText(e.target.value);
+                           }}
+                    />
+                    <button type="search-button" onClick={() => {
+                        console.log(searchText);
+                        console.log(listOfRestaurants);
+                        const filteredRestaurants = listOfRestaurants.filter((res) =>
+                            res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                        setListOfRestaurants(filteredRestaurants);
+                    }}>Search
+                    </button>
+                </div>
                 <button className="filter-btn" onClick={() => {
                     const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
                     setListOfRestaurants(filteredList);
