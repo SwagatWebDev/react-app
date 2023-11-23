83968dc60cf1dc43d804d3d87231b4fadf39224c
swagat030 <swagatkumarmishra888@gmail.com>
2023-11-23 12:28:24 +0530

    File Updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {resList} from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
     // Local State Variable
@@ -12,8 +13,12 @@ const Body = () => {
         const API_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9165757&lng=77.6101163&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
         const data = await fetch(API_URL);
         const response = await data.json();
-        const restaurantListData = response.data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
+        const restaurantListData = response?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         setListOfRestaurants(restaurantListData);
+    }
+    if (listOfRestaurants.length === 0){
+        // return (<h1>Loading...</h1>);
+        return (<Shimmer/>)
     }
     return (
         <div className="body">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {LOGO_URL} from "../utils/constants";
 
 const Header = () => {
