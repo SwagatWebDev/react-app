b52795866391c6e5de4923f8c8dc93f01c6e5936
swagat030 <swagatkumarmishra888@gmail.com>
2023-11-27 02:13:41 +0530

    File Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="company-info">
-                <h2>Foodies</h2>
-                <p>Foodies Deliver: Savor the Flavor, Delivered to Your Doorstep!</p>
-                <p>&copy; 2023 Foodies. All Rights Reserved.</p>
-            </div>
-            <div className="address">
-                <h3>Contact Us</h3>
-                <p>BTM Layout, Bangalore, India</p>
-                <p>Email: info@example.com</p>
-                <p>Phone: +1 (123) 456-7890</p>
+        <div className="footer-container">
+            <div className="footer">
+                <div className="company-info">
+                    <h2>Foodies</h2>
+                    <p>Foodies Deliver: Savor the Flavor, Delivered to Your Doorstep!</p>
+                    <p>&copy; 2023 Foodies. All Rights Reserved.</p>
+                </div>
+                <div className="address">
+                    <h3>Contact Us</h3>
+                    <p>BTM Layout, Bangalore, India</p>
+                    <p>Email: info@example.com</p>
+                    <p>Phone: +1 (123) 456-7890</p>
+                </div>
             </div>
         </div>
     );
--- src/components/Help.js
+++ src/components/Help.js
@@ -1,8 +1,20 @@
 const Help = () => {
     return (
-        <div>
-            <h1>Please inquire bellow for any help</h1>
-            <h2>Help window for Foodies Ltd</h2>
+        <div className="help-page">
+            <h1>Welcome to Foodies Customer Support</h1>
+            <p>
+                If you have any questions or need assistance, our customer support team is here to help you.
+            </p>
+            <h2>Contact Information</h2>
+            <p>
+                You can reach us via email at <strong>support@foodies.com</strong> or by phone at <strong>+1 (123) 456-7890</strong>.
+            </p>
+            <h2>Frequently Asked Questions (FAQs)</h2>
+            <ul>
+                <li>How do I place an order?</li>
+                <li>What payment methods are accepted?</li>
+                <li>How can I track my order?</li>
+            </ul>
         </div>
     )
 };
