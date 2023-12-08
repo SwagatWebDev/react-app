b446842cb51d72edb70d2d60d53da9fe82bf49e7
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-08 14:23:28 +0530

    File Updated

---
+++ src/components/Grocery.js
@@ -0,0 +1,11 @@
+const Grocery = () => {
+    return (
+        <div className="grocery-page">
+            <h1>
+                Our grocery online store and we have a lot of children component inside this web page!!!
+            </h1>
+        </div>
+    )
+}
+
+export default Grocery;
