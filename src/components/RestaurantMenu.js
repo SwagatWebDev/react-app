5f3931b1187c6a634f9e29ddf1dc5b38dfa527e4
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-11 01:28:35 +0530

    tailwind css added and ui enhanced

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,8 +1,6 @@
-import { useEffect } from "react";
-import Shimmer from "./Shimmer";
-import { useParams } from "react-router-dom";
-import { RES_MENU_API_URL } from "../utils/constants";
+import {useParams} from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
@@ -10,41 +8,23 @@ const RestaurantMenu = () => {
 
     if (resInfo === null) return <Shimmer />;
 
-    const { name, cuisines, costForTwoMessage } =
-        resInfo?.cards[0]?.card?.card?.info;
-
-    console.log(resInfo);
-
-    const {
-        itemCards,
-    } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
-        ?.card;
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
+    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
     return (
         <div className="menu p-8">
             <h1 className="text-3xl font-bold mb-4">{name}</h1>
-            <p className="text-gray-600 mb-4">
-                {cuisines.join(", ")} - {costForTwoMessage}
-            </p>
-            <h2 className="text-2xl font-bold mb-4">Menu</h2>
+            <p className="text-gray-600">{cuisines.join(", ")} - {costForTwoMessage}</p>
+            <h2 className="text-2xl font-bold mt-6 mb-4">Menu</h2>
             <ul className="list-disc ml-6">
-                {itemCards &&
-                    itemCards.map((item) => (
-                        <li
-                            key={item.card.info.id}
-                            className="text-lg mb-2 flex justify-between"
-                        >
-                            <span>{item.card.info.name}</span>
-                            <span className="font-bold">
-                                Rs. {item.card.info.price / 100 ||
-                                item.card.info.defaultPrice / 100}
-                            </span>
-                        </li>
-                    ))}
+                {itemCards && itemCards.map((item) => (
+                    <li key={item.card.info.id} className="text-lg mb-2">
+                        {item.card.info.name} - Rs.
+                        {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                    </li>
+                ))}
             </ul>
-            <p className="text-gray-600 mt-4">
-                {!itemCards ? "No Menu Found" : ""}
-            </p>
+            <p className="text-gray-700 mt-4">{!itemCards ? "No Menu Found" : ""}</p>
         </div>
     );
 };
