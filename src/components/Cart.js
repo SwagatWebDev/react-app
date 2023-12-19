c2220e855af224532891d7431b4b9972d6070763
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-19 16:15:26 +0530

    File updated

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,31 @@
-import {useDispatch, useSelector} from "react-redux";
-import RestaurantItemList from "./RestaurantItemList";
-import {clearCart} from "../utils/cartSlice";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RestaurantItemList from './RestaurantItemList';
+import { clearCart } from '../utils/cartSlice';
 
 const Cart = () => {
-
     const cartItems = useSelector((store) => store.cart.items);
-
     const dispatch = useDispatch();
 
     const handleClearCart = () => {
         dispatch(clearCart());
-    }
+    };
+
+    const calculateTotalPrice = () => {
+        let totalPrice = 0;
+        return cartItems.reduce((total, item) => {
+            const itemPrice = item.card.info.price || item.card.info.defaultPrice || 0;
+            totalPrice += itemPrice
+            return Math.round(totalPrice / 100);
+        }, 0);
+    };
+
+
+
+    const handleProceedToCheckout = () => {
+        // Add logic to handle the checkout process
+        console.log('Proceeding to checkout');
+    };
 
     return (
         <div className="cart-page text-center p-8">
@@ -19,17 +34,29 @@ const Cart = () => {
                 <button
                     className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full text-center"
                     onClick={handleClearCart}
-                    disabled={cartItems.length === 0}>
+                    disabled={cartItems.length === 0}
+                >
                     Clear Cart
                 </button>
-                {cartItems.length === 0 && (
-                    <h1 className="text-gray-600 font-bold mt-4">
-                        Cart is empty. Add Items to the cart!
-                    </h1>
+                {cartItems.length === 0 ? (
+                    <h1 className="text-gray-600 font-bold mt-4">Cart is empty. Add Items to the cart!</h1>
+                ) : (
+                    <div>
+                        <RestaurantItemList items={cartItems} />
+                        <div className="mt-4">
+                            <p className="text-lg font-bold">Total Price: ₹{calculateTotalPrice().toFixed(2)}</p>
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center mt-4"
+                                onClick={handleProceedToCheckout}
+                            >
+                                Proceed to Checkout
+                            </button>
+                        </div>
+                    </div>
                 )}
-                <RestaurantItemList items={cartItems}/>
             </div>
         </div>
     );
-}
+};
+
 export default Cart;
