c9fa54a28ffcfa6232d8a0d5f3a4f0577b4b294b
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-20 06:39:38 +0530

    File Updated

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, current} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -6,16 +6,32 @@ const cartSlice = createSlice({
             items: []
         },
     reducers: {
-        // mutating the state here
         addItem: (state, action) => {
-           state.items.push(action.payload);
+            // Vanilla (Older) Redux => DON'T MUTATE THE STATE, RETURN WAS MANDATORY
+            // const newState = (...state)
+            // newState.items.push(action.payload);
+            // return newState
+
+            // Redux Toolkit
+            // HAVE TO MUTATE THE STATE
+            // Redux Toolkit usages Immer Library behind the scene
+            state.items.push(action.payload);
         },
         removeItem: (state, action) => {
             state.items.pop();
         },
+        // original state = ["Burger"]
         clearCart: (state, action) => {
+            // console.log(state);
+            //console.log(current(state));
+            // state = [];
+            // console.log(state);
+
+            // RTK - either Mutate the existing state or return a new state
+            // state.items.length = 0; // originalState = [];
+            // return {items: []}; // this new [] will be replaced inside originalState = []
             state.items.length = 0;
-}
+        }
     }
 })
 
