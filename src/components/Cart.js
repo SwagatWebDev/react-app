b2a204567eca2a0256961badaef33acc49aad619
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-25 12:32:55 +0530

    Latest changes updated

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,62 +1,62 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import RestaurantItemList from './RestaurantItemList';
-import { clearCart } from '../utils/cartSlice';
+import {useDispatch, useSelector} from "react-redux";
+import RestaurantItemList from "./RestaurantItemList";
+import {clearItem} from "../utils/cartSlice";
 
 const Cart = () => {
-    const cartItems = useSelector((store) => store.cart.items);
-    const dispatch = useDispatch();
 
-    const handleClearCart = () => {
-        dispatch(clearCart());
-    };
+    const cartItems = useSelector((store) => store.cart.items);
+
+    // useSelector((store) => store);
+    //
+    // const cartItems = store.cart.item;
+
+    const dispatch = useDispatch();
 
     const calculateTotalPrice = () => {
         let totalPrice = 0;
         return cartItems.reduce((total, item) => {
             const itemPrice = item.card.info.price || item.card.info.defaultPrice || 0;
-            totalPrice += itemPrice
+            totalPrice += itemPrice;
             return Math.round(totalPrice / 100);
-        }, 0);
+        }, 0)
     };
-
-
 
     const handleProceedToCheckout = () => {
-        // Add logic to handle the checkout process
-        console.log('Proceeding to checkout');
-    };
+        console.log('Proceeding Checkout')
+    }
+
+    const handleClearCart = () => {
+        dispatch(clearItem());
+    }
+
+    console.log(cartItems);
 
     return (
         <div className="cart-page text-center p-8">
             <h1 className="text-3xl font-bold mb-4 text-green-600">Cart</h1>
             <div className="w-6/12 mx-auto">
-                <button
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full text-center"
-                    onClick={handleClearCart}
-                    disabled={cartItems.length === 0}
-                >
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 text-center rounded-full"
+                        onClick={handleClearCart}
+                        disabled={cartItems.length === 0}>
                     Clear Cart
                 </button>
-                {cartItems.length === 0 ? (
-                    <h1 className="text-gray-600 font-bold mt-4">Cart is empty. Add Items to the cart!</h1>
-                ) : (
-                    <div>
-                        <RestaurantItemList items={cartItems} />
-                        <div className="mt-4">
-                            <p className="text-lg font-bold">Total Price: ₹{calculateTotalPrice().toFixed(2)}</p>
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center mt-4"
-                                onClick={handleProceedToCheckout}
-                            >
-                                Proceed to Checkout
-                            </button>
+                {cartItems.length === 0 ? (<h1 className="text-gray-600 font-bold mt-4">Cart is empty. Add Items to the cart!</h1>):
+                    (
+                        <div>
+                            <RestaurantItemList items={cartItems}/>
+                            <div className="mt-4">
+                                <p className="text-lg font-bold">Total Price: ₹{calculateTotalPrice().toFixed(2)}</p>
+                                <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full text-center mt-4 hover:bg-blue-700"
+                                        onClick={handleProceedToCheckout}>
+                                    Proceed to checkout
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )
+                }
             </div>
         </div>
     );
-};
+}
 
 export default Cart;
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -2,39 +2,39 @@ import {createSlice, current} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name: 'cart',
-        initialState: {
-            items: []
-        },
+    initialState: {
+        items: []
+    },
     reducers: {
-        addItem: (state, action) => {
-            // Vanilla (Older) Redux => DON'T MUTATE THE STATE, RETURN WAS MANDATORY
-            // const newState = (...state)
+        addItem: (state, action)  => {
+            // Vanilla (Older) Version Redux => DON'T MUTATE THE STATE
+            // console.log(state);
+            // console.log(current(state));
+            // console.log(action);
+            // const newState = ...state;
             // newState.items.push(action.payload);
-            // return newState
+            // return newState;
 
-            // Redux Toolkit
-            // HAVE TO MUTATE THE STATE
-            // Redux Toolkit usages Immer Library behind the scene
-            state.items.push(action.payload);
+            // REDUX Toolkit
+            // mutate the state
+            state.items.push(action.payload)
+            // console.log(current(state));
         },
         removeItem: (state, action) => {
             state.items.pop();
         },
-        // original state = ["Burger"]
-        clearCart: (state, action) => {
+        clearItem: (state, action) => {
             // console.log(state);
-            //console.log(current(state));
+            // console.log(current(state));
             // state = [];
             // console.log(state);
-
             // RTK - either Mutate the existing state or return a new state
-            // state.items.length = 0; // originalState = [];
-            // return {items: []}; // this new [] will be replaced inside originalState = []
-            state.items.length = 0;
+            //return {items: state};
+            state.items.length = 0; // state = [];
         }
     }
 })
 
-export const {addItem, removeItem, clearCart} = cartSlice.actions;
 
+export const {addItem, removeItem, clearItem} = cartSlice.actions;
 export default cartSlice.reducer;
